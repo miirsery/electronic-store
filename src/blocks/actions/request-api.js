ac0c1c9546f4c64e2f06f129e56bc99c6ba83601
Андрей Голубев <kpdpav@gmail.com>
2021-11-16 20:21:17 +0300

    add js create product and order

--- src/blocks/actions/request-api.js
+++ src/blocks/actions/request-api.js
@@ -1,5 +1,38 @@
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function handlerOrder(formData) {
+    axios.post(
+        '/cart/create-order/',
+        formData,
+        {headers: {
+            'X-CSRFToken': getCookie('csrftoken')
+        }}
+    )
+}
+
+function handlerCartAddProduct(id) {
+    axios.post(
+        '/cart/add-product/',
+        {id: id},
+        {headers: {
+            'X-CSRFToken': getCookie('csrftoken')
+        }}
+    )
+}
+
 document.addEventListener('click' , ({target: {id , tagName, className}}) => {
     if(tagName === 'BUTTON' && className === 'product__item-add-button') {
-        console.log(id)
+        handlerCartAddProduct(id)
     }
 })
+
+document.querySelector('.order__item-form').addEventListener('submit', (e) => {
+    console.log('OKEY')
+    const formData = Object.fromEntries(new FormData(e.target).entries())
+    handlerOrder(formData)
+    e.preventDefault()
+  });
