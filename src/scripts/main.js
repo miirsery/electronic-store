cceee550df91b08d443cbd93a643c764f1b78cc3
miirsery <miirsery.main@gmail.com>
2021-11-18 06:31:08 +0700

    fix header menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 import '../blocks/block'
 
 import 'swiper/swiper.scss'
+// import { Axios } from 'axios'
+// import 'require/require'
 // import "swiper/css";
 
 // import 'swiper/components/effect-fade/effect-fade.scss'
